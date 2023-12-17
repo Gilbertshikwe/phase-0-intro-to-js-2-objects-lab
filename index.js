5a776bc6461a4392c5f668f6074b7f057cddfcfa
Gilbert <shikweg@gmail.com>
2023-12-17 11:12:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Gilbert Shikwe",
+    streetAdress: "11 Broadway"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+ return {
+    ...employee, 
+    [key]:value
+ }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// Function to delete a key from employee object (non-destructive)
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// Function to destructively delete a key from employee object
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
